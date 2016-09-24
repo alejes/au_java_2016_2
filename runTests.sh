e955fdce15d95b4ce7c24610f97783e13daf525f
alejes <alejes@yandex.ru>
2016-09-24 13:00:16 +0300

    add tests to gradle clean, add new merge test

--- runTests.sh
+++ runTests.sh
@@ -5,9 +5,10 @@ mkdir -p $WORKDIR
 
 echo "Log test"
 
-echo "file2" >> $WORKDIR/file2.txt
-echo "file1" >> $WORKDIR/file1.txt
 cd $WORKDIR
+echo "file2" >> file2.txt
+echo "file1" >> file1.txt
+
 java -jar ../../build/libs/vcs-1.0-SNAPSHOT.jar vcs init
 java -jar ../../build/libs/vcs-1.0-SNAPSHOT.jar vcs commit "first"
 java -jar ../../build/libs/vcs-1.0-SNAPSHOT.jar vcs commit "second"
@@ -77,4 +78,26 @@ echo "file1_!from" >> file1.txt
 echo "file1_!from2" >> file1.txt
 java -jar ../../build/libs/vcs-1.0-SNAPSHOT.jar vcs merge "branch"
 echo -ne 'file1\n+file1_!from\n-file1_!\n+file1_!from2' > file1.diff
+diff file1.diff file1.txt
+
+
+cd .. && rm -rf workdir && mkdir workdir && cd workdir
+echo "Merge test 2"
+echo "file1" >> file1.txt
+echo "file2" >> file2.txt
+java -jar ../../build/libs/vcs-1.0-SNAPSHOT.jar vcs init
+java -jar ../../build/libs/vcs-1.0-SNAPSHOT.jar vcs commit "first"
+java -jar ../../build/libs/vcs-1.0-SNAPSHOT.jar vcs checkout -b "branch"
+echo "file1_!" >> file1.txt
+echo "file1_2" >> file1.txt
+echo "file1_common1" >> file1.txt
+echo "file1_common2" >> file1.txt
+java -jar ../../build/libs/vcs-1.0-SNAPSHOT.jar vcs commit "branchcommit"
+java -jar ../../build/libs/vcs-1.0-SNAPSHOT.jar vcs checkout "master"
+echo "file1_!from" >> file1.txt
+echo "file1_!from2" >> file1.txt
+echo "file1_common1" >> file1.txt
+echo "file1_common2" >> file1.txt
+java -jar ../../build/libs/vcs-1.0-SNAPSHOT.jar vcs merge "branch"
+echo -ne 'file1\n+file1_!from\n+file1_!from2\n-file1_!\n-file1_2\nfile1_common1\nfile1_common2' > file1.diff
 diff file1.diff file1.txt
